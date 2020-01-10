2441088a6b8664cf70ece52725d0fa77e86943b3
Paola <Paola@PC-PaolaLaMagnifica>
2020-01-10 15:54:24 +0100

    Finito

--- src/it/polito/tdp/quadrato/CercaQuadrato.java
+++ src/it/polito/tdp/quadrato/CercaQuadrato.java
@@ -1,25 +1,35 @@
 package it.polito.tdp.quadrato;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CercaQuadrato {
 
+	public CercaQuadrato() {
+	}
+
 	List<Quadrato> soluzioni;
-
+	
+	public void genera(int size) {
+		Quadrato parziale = new Quadrato(size) ;
+		this.soluzioni= new ArrayList<Quadrato>() ;
+		this.cerca(parziale, 0);
+		
+	}
+	
 	private void cerca(Quadrato parziale, int L) {
-		// le L caselle da 0 a L-1 sono piene
-		// devo trovare un valore per la casella
-		// in posizione L
-
-		if (L == parziale.getSize() * parziale.getSize()) {
-			// il quadrato Ã¨ tutto pieno
-			// devo verificare se Ã¨ 'magico'
+		
+		if(L == parziale.getSize()*parziale.getSize()) { //il quadrato è completo se il livello è uguale alla dimensione del quadrato
+			
 			if (parziale.isMagic()) {
+				System.out.println(parziale) ;
 				soluzioni.add(parziale.clone());
 			}
 			return;
 		}
-
+		
+		//altri casi
+			
 		for (int i = 1; i <= parziale.getSize() * parziale.getSize(); i++) {
 			// provo a mettere il valore 'i' nella casella 'L'
 
@@ -32,5 +42,5 @@ public class CercaQuadrato {
 			}
 		}
 	}
-
+	
 }
---
+++ src/it/polito/tdp/quadrato/TestQuadrato.java
@@ -0,0 +1,13 @@
+package it.polito.tdp.quadrato;
+
+public class TestQuadrato {
+	
+	public static void main(String[] args) {
+
+		CercaQuadrato cq = new CercaQuadrato() ;
+		
+		cq.genera(3) ;
+		
+		
+	}
+}
